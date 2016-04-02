c38c44696b112981902c1d208fd8f7b95f421139
Zac Barton <zac.barton@staff.atmail.com>
2016-04-02 21:03:41 +1000

    use flushHeaders() over deprecated flush().

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 function sse(req, res, next) {
-	req.socket.setKeepAlive(true);	
-	req.socket.setTimeout(0);
+  req.socket.setKeepAlive(true);
+  req.socket.setTimeout(0);
 
-	res.setHeader('Content-Type', 'text/event-stream');
-	res.setHeader('Cache-Control', 'no-cache');
-	res.setHeader('Connection', 'keep-alive');
-	res.status(200);
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.status(200);
 
-	// export a function to send server-side-events
-	res.sse = function(string) {
-		res.write(string);
-		
-		// support running within the compression middleware
-		if (res.flush && string.match(/\n\n$/)) {
-			res.flush();
-		}
-	};
+  // export a function to send server-side-events
+  res.sse = function sse(string) {
+    res.write(string);
 
-	// write 2kB of padding (for IE) and a reconnection timeout
-	// then use res.sse to send to the client
-	res.write(':' + Array(2049).join(' ') + '\n');
-	res.sse('retry: 2000\n\n');
+    // support running within the compression middleware
+    if (res.flushHeaders && string.match(/\n\n$/)) {
+      res.flushHeaders();
+    }
+  };
 
-	// keep the connection open by sending a comment
-	var keepAlive = setInterval(function() {
-    	res.sse(':keep-alive\n\n');
-	}, 20000);
+  // write 2kB of padding (for IE) and a reconnection timeout
+  // then use res.sse to send to the client
+  res.write(':' + Array(2049).join(' ') + '\n');
+  res.sse('retry: 2000\n\n');
 
-	// cleanup on close
-	res.on('close', function() {
-		clearInterval(keepAlive);
-	});
+  // keep the connection open by sending a comment
+  var keepAlive = setInterval(function() {
+    res.sse(':keep-alive\n\n');
+  }, 20000);
 
-	next();
+  // cleanup on close
+  res.on('close', function close() {
+    clearInterval(keepAlive);
+  });
+
+  next();
 }
 
 module.exports = sse;
